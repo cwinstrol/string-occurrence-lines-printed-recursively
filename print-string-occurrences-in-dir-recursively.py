04e756d8cbbdade40d478d9827b8cdf3a8241d72
James Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2021-01-12 22:36:52 -0500

    Update print-string-occurrences-in-dir-recursively.py

--- print-string-occurrences-in-dir-recursively.py
+++ print-string-occurrences-in-dir-recursively.py
@@ -3,8 +3,8 @@ dir=('') #edit this string to name the directory to be recursively searched
 for folder, dirs, files in os.walk(dir):
     for file in files:
         if file.endswith('.txt'): #editable for other file extension types such as html, csv, etc.
-            fullpath = os.path.join(folder, file)
-            with open(fullpath, 'r') as opened:
+            isOfType = os.path.join(folder, file)
+            with open(isOfType, 'r') as opened:
                 for line in opened:
                     if '' in line: #declare the string to be found
                         print(line)
